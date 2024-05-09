fe309496f12bff2c25d51ef985228f5ac12e10d0
badcrazy <badcrazy@gmail.com>
2024-05-10 00:45:28 +0800

    Update diy-part1.sh

--- scripts/zn-m2/diy-part1.sh
+++ scripts/zn-m2/diy-part1.sh
@@ -9,9 +9,10 @@
 # File name: diy-part1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
-
+rm -rf feeds/luci/applications/luci-app-ssr-plus
 # Uncomment a feed source
 #echo 'src-git custom https://github.com/xiaorouji/openwrt-passwall.git;packages' >feeds.conf.default
 git clone https://github.com/kongfl888/luci-app-adguardhome package/luci-app-adguardhome
 # git clone https://github.com/zhengmz/luci-app-zerotier package/luci-app-zerotier
 git clone https://github.com/sirpdboy/netspeedtest package/luci-app-netspeedtest
+git clone https://github.com/Mattraks/helloworld package/luci-app-ssr-plus
