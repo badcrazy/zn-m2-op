9abb452a24da6fa8b3bc0181b06f94d4ba2c4a85
badcrazy <badcrazy@gmail.com>
2024-11-11 00:16:33 +0800

    Update diy-part1.sh

--- scripts/zn-m2/diy-part1.sh
+++ scripts/zn-m2/diy-part1.sh
@@ -11,14 +11,14 @@
 #
 # Uncomment a feed source
 # 移除要替换的包
-rm -rf feeds/luci/applications/luci-app-zerotier
+# rm -rf feeds/luci/applications/luci-app-zerotier
 rm -rf feeds/packages/net/zerotier
 # 添加额外插件
 git clone https://github.com/kongfl888/luci-app-adguardhome package/luci-app-adguardhome
 # git clone https://github.com/zhengmz/luci-app-zerotier package/luci-app-zerotier
 git clone https://github.com/sirpdboy/netspeedtest package/luci-app-netspeedtest
 git clone https://github.com/mwarning/zerotier-openwrt package/zerotier-openwrt
-git clone https://github.com/zhengmz/luci-app-zerotier package/luci-app-zerotier
+# git clone https://github.com/zhengmz/luci-app-zerotier package/luci-app-zerotier
 
 # 科学上网插件
 # git clone https://github.com/fw876/helloworld package/luci-app-ssr-plus
