438bd2a2302dabd364b14dca08409eb88ae89ffa
badcrazy <badcrazy@gmail.com>
2024-11-10 18:23:30 +0800

    Update diy-part1.sh

--- scripts/zn-m2/diy-part1.sh
+++ scripts/zn-m2/diy-part1.sh
@@ -15,8 +15,7 @@
 # 添加额外插件
 git clone https://github.com/kongfl888/luci-app-adguardhome package/luci-app-adguardhome
 # git clone https://github.com/zhengmz/luci-app-zerotier package/luci-app-zerotier
-# git clone https://github.com/sirpdboy/netspeedtest package/luci-app-netspeedtest
-git clone https://github.com/selfcan/luci-app-homebox  package/luci-app-homebox
+git clone https://github.com/sirpdboy/netspeedtest package/luci-app-netspeedtest
 
 # 科学上网插件
 # git clone https://github.com/fw876/helloworld package/luci-app-ssr-plus
