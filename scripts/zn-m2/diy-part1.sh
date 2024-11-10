0d1ef6ba444024b27dd9f976c30f571f4483ceed
badcrazy <badcrazy@gmail.com>
2024-11-10 12:43:21 +0800

    Update diy-part1.sh

--- scripts/zn-m2/diy-part1.sh
+++ scripts/zn-m2/diy-part1.sh
@@ -11,7 +11,9 @@
 #
 # Uncomment a feed source
 # 移除要替换的包
-
+rm -rf feeds/luci/applications/luci-app-ssr-plus
+rm -rf feeds/luci/applications/luci-app-ssr-libev-server
+rm -rf feeds/luci/applications/luci-app-ssr-mudb-server
 # 添加额外插件
 git clone https://github.com/kongfl888/luci-app-adguardhome package/luci-app-adguardhome
 # git clone https://github.com/zhengmz/luci-app-zerotier package/luci-app-zerotier
