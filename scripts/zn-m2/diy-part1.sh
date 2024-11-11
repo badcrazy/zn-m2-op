630c6c64db2c33757cc7c11b213068c6c8e5fc86
badcrazy <badcrazy@gmail.com>
2024-11-11 21:38:42 +0800

    Update diy-part1.sh

--- scripts/zn-m2/diy-part1.sh
+++ scripts/zn-m2/diy-part1.sh
@@ -17,8 +17,8 @@ rm -rf feeds/packages/net/zerotier
 git clone https://github.com/kongfl888/luci-app-adguardhome package/luci-app-adguardhome
 # git clone https://github.com/zhengmz/luci-app-zerotier package/luci-app-zerotier
 # git clone https://github.com/sirpdboy/netspeedtest package/luci-app-netspeedtest
-# git clone https://github.com/coolsnowwolf/packages/tree/98b47a0b181800ae4478f630e76f1f8232987199/net/zerotier package/zerotier
-# git clone https://github.com/coolsnowwolf/luci/tree/a1e6216966bfa90b7c45aef8dbe1e8c008db791b/applications/luci-app-zerotier package/luci-app-zerotier
+git clone https://github.com/coolsnowwolf/packages/tree/98b47a0b181800ae4478f630e76f1f8232987199/net/zerotier package/zerotier
+git clone https://github.com/coolsnowwolf/luci/tree/a1e6216966bfa90b7c45aef8dbe1e8c008db791b/applications/luci-app-zerotier package/luci-app-zerotier
 
 # 科学上网插件
 # git clone https://github.com/fw876/helloworld package/luci-app-ssr-plus
