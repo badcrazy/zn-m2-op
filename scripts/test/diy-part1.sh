858bc1dbf2126337daab40125268d62b068ef068
badcrazy <badcrazy@gmail.com>
2024-12-06 22:12:56 +0800

    Update diy-part1.sh

--- scripts/test/diy-part1.sh
+++ scripts/test/diy-part1.sh
@@ -11,17 +11,9 @@
 #
 # Uncomment a feed source
 # 移除要替换的包
-rm -rf feeds/packages/net/mosdns
-rm -rf feeds/packages/net/msd_lite
-rm -rf feeds/packages/net/smartdns
-rm -rf feeds/luci/themes/luci-theme-argon
-rm -rf feeds/luci/themes/luci-theme-netgear
-rm -rf feeds/luci/applications/luci-app-mosdns
-rm -rf feeds/luci/applications/luci-app-netdata
-rm -rf feeds/luci/applications/luci-app-serverchan
 rm -rf feeds/luci/themes/luci-theme-bootstrap
 rm -rf feeds/luci/applications/luci-app-upnp/
 # 添加额外插件
 git clone https://github.com/badcrazy/mypackage feeds/package
 # 取消主题默认设置
-find package/luci-theme-*/* -type f -name '*luci-theme-*' -print -exec sed -i '/set luci.main.mediaurlbase/d' {} \;
+# find package/luci-theme-*/* -type f -name '*luci-theme-*' -print -exec sed -i '/set luci.main.mediaurlbase/d' {} \;
